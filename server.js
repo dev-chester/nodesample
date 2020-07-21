4bad6f1a69449972077dce14290756f1e5716c2d
dev-chester <chesterarellano1@gmail.com>
2020-07-21 15:30:19 +0800

    [fix] to auto build image

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var port = process.env.PORT || 3000;
 
 router.get('/Hello', function(req, res) {
     var envName = `${process.env.FIRSTNAME} ${process.env.LASTNAME}`;
-    res.json({ message: 'Hello docker-compose!', name:  envName});   
+    res.json({ message: 'Hello docker-hub!', name:  envName});   
 });
 
 router.post('/Post', (req,res) => {
